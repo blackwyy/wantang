f716cb7f15c1f56b6dcf49c5c30a0aa2498b647d
wangyang <wangyang573807932@163.com>
2022-05-02 21:57:57 +0800

    错误信息

---
+++ lang/zh_CN/validation.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    'attributes' => [
+        'name' => '名称',
+        'email' => '邮箱',
+        'password' => '密码',
+    ],
+
+    'custom' => [
+        'email' => [
+            'required' => '邮箱地址不能为空！',
+        ],
+    ],
+
+];
